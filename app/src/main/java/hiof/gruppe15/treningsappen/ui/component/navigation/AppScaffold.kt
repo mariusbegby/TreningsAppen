67e5e8151975c706114404a3555e123a5479d383
Marius Begby <mariusbegby@gmail.com>
2023-11-16 17:32:41 +0100

    Redesign bottom bar and add text to icons

--- app/src/main/java/hiof/gruppe15/treningsappen/ui/component/navigation/AppScaffold.kt
+++ app/src/main/java/hiof/gruppe15/treningsappen/ui/component/navigation/AppScaffold.kt
@@ -1,16 +1,18 @@
 package hiof.gruppe15.treningsappen.ui.component.navigation
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Home
@@ -19,6 +21,7 @@ import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -26,7 +29,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
@@ -38,34 +41,31 @@ import com.example.treningsappen.R
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppScaffold(
-    navController: NavController,
-    title: String,
-    content: @Composable (PaddingValues) -> Unit
+    navController: NavController, title: String, content: @Composable (PaddingValues) -> Unit
 ) {
-    Scaffold(
-        topBar = {
-            TopAppBar(title = { Text(title) }, navigationIcon = {
-                IconButton(onClick = {
-                    navController.navigateUp()
-                }) {
-                    Icon(Icons.Filled.ArrowBack, contentDescription = "Go back")
-                }
-            })
-        },
-        bottomBar = {
-            BottomAppBar(
-                containerColor = Color.Unspecified, modifier = Modifier.background(Color.White)
-            ) {
-                BottomNavigationRow(navController)
+    Scaffold(topBar = {
+        TopAppBar(title = { Text(title) }, navigationIcon = {
+            IconButton(onClick = {
+                navController.navigateUp()
+            }) {
+                Icon(Icons.Filled.ArrowBack, contentDescription = "Go back")
             }
+        })
+    }, bottomBar = {
+        BottomAppBar(
+            containerColor = MaterialTheme.colorScheme.background
+        ) {
+            BottomNavigationRow(navController)
         }
-    ) { innerPadding ->
-        content(PaddingValues(
-            top = innerPadding.calculateTopPadding(),
-            bottom = innerPadding.calculateBottomPadding(),
-            start = 16.dp,
-            end = 16.dp
-        ))
+    }) { innerPadding ->
+        content(
+            PaddingValues(
+                top = innerPadding.calculateTopPadding(),
+                bottom = innerPadding.calculateBottomPadding(),
+                start = 16.dp,
+                end = 16.dp
+            )
+        )
     }
 }
 
@@ -80,16 +80,24 @@ private fun BottomNavigationRow(navController: NavController) {
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        HomeNavButton(navController, isSelected = isRouteSelected(currentRoute, ScreenCategory.Home))
+        HomeNavButton(
+            navController, isSelected = isRouteSelected(currentRoute, ScreenCategory.Home)
+        )
         Spacer(modifier = Modifier.width(12.dp))
 
-        WorkoutPlanNavButton(navController, isSelected = isRouteSelected(currentRoute, ScreenCategory.Workout))
+        WorkoutPlanNavButton(
+            navController, isSelected = isRouteSelected(currentRoute, ScreenCategory.Workout)
+        )
         Spacer(modifier = Modifier.width(12.dp))
 
-        AnalyticsNavButton(navController, isSelected = isRouteSelected(currentRoute, ScreenCategory.Analytics))
+        AnalyticsNavButton(
+            navController, isSelected = isRouteSelected(currentRoute, ScreenCategory.Analytics)
+        )
         Spacer(modifier = Modifier.width(12.dp))
 
-        ProfileNavButton(navController, isSelected = isRouteSelected(currentRoute, ScreenCategory.Profile))
+        ProfileNavButton(
+            navController, isSelected = isRouteSelected(currentRoute, ScreenCategory.Profile)
+        )
     }
 }
 
@@ -100,7 +108,8 @@ private fun HomeNavButton(navController: NavController, isSelected: Boolean) {
         isSelected = isSelected,
         iconWrapper = IconWrapper.ImageVectorIcon(Icons.Default.Home),
         contentDescription = "Home Icon",
-        route = Screen.Home.route
+        route = Screen.Home.route,
+        label = "Home"
     )
 }
 
@@ -111,7 +120,8 @@ private fun WorkoutPlanNavButton(navController: NavController, isSelected: Boole
         isSelected = isSelected,
         iconWrapper = IconWrapper.PainterIcon(painterResource(id = R.drawable.fitness)),
         contentDescription = "Barbell Dumbell Icon",
-        route = Screen.WorkoutPlan.route
+        route = Screen.WorkoutPlan.route,
+        label = "Routines"
     )
 }
 
@@ -123,7 +133,8 @@ private fun AnalyticsNavButton(navController: NavController, isSelected: Boolean
         isSelected = isSelected,
         iconWrapper = IconWrapper.PainterIcon(painterResource(id = R.drawable.trendingup)),
         contentDescription = "Arrow Chart Increase Icon",
-        route = Screen.Analytics.route
+        route = Screen.Analytics.route,
+        label = "Analytics"
     )
 }
 
@@ -134,7 +145,8 @@ private fun ProfileNavButton(navController: NavController, isSelected: Boolean) 
         isSelected = isSelected,
         iconWrapper = IconWrapper.ImageVectorIcon(Icons.Default.Person),
         contentDescription = "Person Icon",
-        route = Screen.Profile.route
+        route = Screen.Profile.route,
+        label = "Profile"
     )
 }
 
@@ -144,34 +156,49 @@ private fun NavButton(
     isSelected: Boolean,
     iconWrapper: IconWrapper,
     contentDescription: String,
-    route: String
+    route: String,
+    label: String
 ) {
-    val backgroundColor = if (isSelected) Color.Black else Color.White
-    val iconColor = if (isSelected) Color.White else Color.Black
+    val selectedColor =
+        if (isSelected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onPrimaryContainer
 
     Box(
         modifier = Modifier
-            .size(30.dp)
-            .background(backgroundColor, shape = CircleShape)
-            .padding(1.dp)
-    ) {
-        IconButton(onClick = {
-            navController.navigate(route) {
-                popUpTo(route) { inclusive = true }
+            .clip(RoundedCornerShape(12.dp))
+            .clickable {
+                navController.navigate(route) {
+                    popUpTo(route) { inclusive = true }
+                }
             }
-        }) {
+            .padding(8.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
             when (iconWrapper) {
                 is IconWrapper.ImageVectorIcon -> Icon(
                     imageVector = iconWrapper.imageVector,
                     contentDescription = contentDescription,
-                    tint = iconColor
+                    tint = selectedColor,
+                    modifier = Modifier.size(24.dp) // Control the size of the Icon
                 )
                 is IconWrapper.PainterIcon -> Icon(
                     painter = iconWrapper.painter,
                     contentDescription = contentDescription,
-                    tint = iconColor
+                    tint = selectedColor,
+                    modifier = Modifier.size(24.dp) // Control the size of the Icon
                 )
             }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = label,
+                style = MaterialTheme.typography.labelMedium,
+                color = selectedColor
+            )
         }
     }
 }
