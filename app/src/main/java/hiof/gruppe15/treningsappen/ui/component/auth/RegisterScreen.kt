ffdb52e71e5d03e6fe2412a30900e8b0332941c7
Marius Begby <mariusbegby@gmail.com>
2023-11-16 16:52:17 +0100

    Design new registration screen

--- app/src/main/java/hiof/gruppe15/treningsappen/ui/component/auth/RegisterScreen.kt
+++ app/src/main/java/hiof/gruppe15/treningsappen/ui/component/auth/RegisterScreen.kt
@@ -1,17 +1,15 @@
 package hiof.gruppe15.treningsappen.ui.component.auth
 
 import android.widget.Toast
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -20,18 +18,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.treningsappen.R
 import com.google.firebase.auth.FirebaseAuth
 import hiof.gruppe15.treningsappen.ui.component.navigation.Screen
 import hiof.gruppe15.treningsappen.utils.ValidationUtils
@@ -44,83 +34,96 @@ fun RegisterScreen(navController: NavController) {
     var password by remember { mutableStateOf("") }
     var passwordVisibility by remember { mutableStateOf(false) }
 
-    val emailFocusRequester = remember { FocusRequester() }
-    val passwordFocusRequester = remember { FocusRequester() }
+    TopBar(navController)
 
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.padding(16.dp)
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(start = 16.dp, end = 16.dp),
+        contentAlignment = Alignment.Center
     ) {
-        OutlinedTextField(value = email,
-            onValueChange = { email = it },
-            label = { Text("Email address") },
-            singleLine = true,
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Email, imeAction = ImeAction.Next
-            ),
-            keyboardActions = KeyboardActions(onNext = { passwordFocusRequester.requestFocus() }),
-            modifier = Modifier.focusRequester(emailFocusRequester)
-        )
-        Spacer(modifier = Modifier.height(16.dp))
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Spacer(modifier = Modifier.height(96.dp))
 
-        OutlinedTextField(value = password,
-            onValueChange = { password = it },
-            label = { Text("Password") },
-            singleLine = true,
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Password,
-                imeAction = ImeAction.Done,
-            ),
-            visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
-            trailingIcon = {
-                Icon(painter = painterResource(id = R.drawable.baseline_eye_24),
-                    contentDescription = "Toggle password visibility",
-                    tint = Color.Gray,
-                    modifier = Modifier.clickable { passwordVisibility = !passwordVisibility })
-            },
-            modifier = Modifier.focusRequester(passwordFocusRequester)
-        )
-        Spacer(modifier = Modifier.height(16.dp))
+            AppLogo()
 
-        Button(onClick = {
-            if (email.isNotEmpty() && password.isNotEmpty()) {
-                if (ValidationUtils.isValidEmail(email)) {
-                    if (ValidationUtils.isPasswordStrong(password)) {
-                        auth.createUserWithEmailAndPassword(email, password)
-                            .addOnCompleteListener { task ->
-                                if (task.isSuccessful) {
-                                    Toast.makeText(
-                                        context, "Registration successful", Toast.LENGTH_SHORT
-                                    ).show()
-                                    navController.navigate(Screen.Login.route) {
-                                        popUpTo(Screen.Register.route) { inclusive = true }
+            Spacer(modifier = Modifier.height(24.dp))
+
+            TitleTexts("Create Account", "Sign up to get started")
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            EmailInputField(email, onEmailChange = { email = it }) {
+
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            PasswordInputField(password, onPasswordChange = { password = it }, passwordVisibility) {
+                passwordVisibility = !passwordVisibility
+            }
+
+            Spacer(modifier = Modifier.height(36.dp))
+
+            RegisterButton(onRegisterClick = {
+                if (email.isNotEmpty() && password.isNotEmpty()) {
+                    if (ValidationUtils.isValidEmail(email)) {
+                        if (ValidationUtils.isPasswordStrong(password)) {
+                            auth.createUserWithEmailAndPassword(email, password)
+                                .addOnCompleteListener { task ->
+                                    if (task.isSuccessful) {
+                                        Toast.makeText(
+                                            context, "Registration successful", Toast.LENGTH_SHORT
+                                        ).show()
+                                        navController.navigate(Screen.Login.route) {
+                                            popUpTo(Screen.Register.route) { inclusive = true }
+                                        }
+                                    } else {
+                                        Toast.makeText(
+                                            context,
+                                            "Registration failed: ${task.exception?.message}",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
                                     }
-                                } else {
-                                    Toast.makeText(
-                                        context,
-                                        "Registration failed: ${task.exception?.message}",
-                                        Toast.LENGTH_SHORT
-                                    ).show()
                                 }
-                            }
+                        } else {
+                            Toast.makeText(
+                                context,
+                                "Password should be at least 8 characters and contain a mix of letters and numbers",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
                     } else {
                         Toast.makeText(
-                            context,
-                            "Password should be at least 8 characters and contain a mix of letters and numbers",
-                            Toast.LENGTH_SHORT
+                            context, "Please enter a valid email address", Toast.LENGTH_SHORT
                         ).show()
                     }
                 } else {
                     Toast.makeText(
-                        context, "Please enter a valid email address", Toast.LENGTH_SHORT
+                        context, "Please enter your email and password", Toast.LENGTH_SHORT
                     ).show()
                 }
-            } else {
-                Toast.makeText(context, "Please fill in all fields", Toast.LENGTH_SHORT).show()
-            }
-        }) {
-            Text(text = "Register")
+            })
+
+            Spacer(modifier = Modifier.weight(1f))
         }
+    }
+}
+
+@Composable
+fun RegisterButton(onRegisterClick: () -> Unit) {
+    Button(
+        onClick = onRegisterClick, modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp)
+    ) {
+        Text(
+            text = "Register", color = Color.White, style = MaterialTheme.typography.titleMedium
+        )
     }
 }
