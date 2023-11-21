7a8b4afb8091813335526a533b43b552eb676d9c
Marius Begby <mariusbegby@gmail.com>
2023-11-21 14:57:12 +0100

    Move create routine button from home to routines

--- app/src/main/java/hiof/gruppe15/treningsappen/ui/component/home/Home.kt
+++ app/src/main/java/hiof/gruppe15/treningsappen/ui/component/home/Home.kt
@@ -2,7 +2,6 @@ package hiof.gruppe15.treningsappen.ui.component.home
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -10,7 +9,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import hiof.gruppe15.treningsappen.ui.component.navigation.AppScaffold
 import hiof.gruppe15.treningsappen.ui.component.navigation.Screen
@@ -25,14 +23,6 @@ fun HomeScreen(navController: NavController) {
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Button(onClick = {
-                navController.navigate(Screen.CreateNewRoutine.route)
-            }) {
-                Text("Create Routine")
-            }
-
-            Spacer(modifier = Modifier.padding(8.dp))
-
             Button(onClick = {
                 navController.navigate(Screen.Testing.route)
             }) {
