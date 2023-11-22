d39f5d96e3a9e043da2406b57a307d284dd4bf65
Daniel N Johansen <113354476+Danieljoh98@users.noreply.github.com>
2023-11-22 05:14:59 +0100

    add profile image test1
    ikke add før vi snakker sammen

--- app/src/main/java/hiof/gruppe15/treningsappen/ui/component/profile/Profile.kt
+++ app/src/main/java/hiof/gruppe15/treningsappen/ui/component/profile/Profile.kt
@@ -1,22 +1,43 @@
 package hiof.gruppe15.treningsappen.ui.component.profile
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
 import hiof.gruppe15.treningsappen.ui.component.navigation.AppScaffold
 import hiof.gruppe15.treningsappen.ui.component.navigation.Screen
 import hiof.gruppe15.treningsappen.viewmodel.SharedViewModel
@@ -66,6 +87,77 @@ fun ProfileScreen(navController: NavController, sharedViewModel: SharedViewModel
             })
 
             Spacer(modifier = Modifier.height(24.dp))
+
+            AddProfileImageButton { uri ->
+                // Save the selected image URI to the user's profile
+                //saveImageToUserProfile(uri)
+                // Handle the selected image URI as needed
+                // For now, let's print the URI
+                println("Selected Image URI: $uri")
+            }
+        }
+    }
+}
+
+@Composable
+fun AddProfileImageButton(
+    onImageAdded: (Uri) -> Unit
+) {
+    var selectedImageUri by remember { mutableStateOf<Uri?>(null) }
+    var isImagePickerVisible by remember { mutableStateOf(false) }
+
+    // Function to save the image URI to the user's profile
+    val saveImageToUserProfile: (Uri) -> Unit = { uri ->
+        val userId = FirebaseAuth.getInstance().currentUser?.uid
+        if (userId != null) {
+            val database = FirebaseDatabase.getInstance().reference
+            database.child("users").child(userId).child("profileImageUri").setValue(uri.toString())
+        }
+        onImageAdded(uri)
+    }
+
+    val getContent = rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        if (uri != null) {
+            selectedImageUri = uri
+            // Save the selected image URI to the user's profile
+            saveImageToUserProfile(uri)
+            // Call the callback to handle the selected image URI
+            onImageAdded(uri)
+        }
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        selectedImageUri?.let { uri ->
+            Image(
+                painter = rememberImagePainter(uri),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(30.dp)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.primary, shape = CircleShape)
+                    .aspectRatio(0.75f)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                getContent.launch("image/*")
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(48.dp)
+        ) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = "Add Profile Picture", style = MaterialTheme.typography.titleMedium)
         }
     }
 }
